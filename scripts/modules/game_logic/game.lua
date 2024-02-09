b9b9fc8e0829bf31185b3816fdc847fb53ec6e93
Alexey Ukhov <alekcey@tut.by>
2024-02-09 13:46:17 +0300

    Rename.

--- scripts/modules/game_logic/game.lua
+++
@@ -1,7 +0,0 @@
-local M ={}
-
-function M.start_level(self)
-	print("start_level")
-end
-
-return M
---
+++ scripts/modules/game_logic/game_fsm.lua
@@ -0,0 +1,7 @@
+local M ={}
+
+function M.init(self)
+	print("init fsm", self.level)
+end
+
+return M
